8c6fb92f1a14a0e822d10f793a17e504d2669756
i.kłusek <J6ZkqwVUn>
2023-03-03 15:01:59 +0100

    ShoppinList services

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,1 +1,20 @@
-export class ShoppingListService {}
+import { EventEmitter } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
+
+export class ShoppingListService {
+  ingredientsChanged = new EventEmitter<Ingredient[]>();
+  private ingredients: Ingredient[] = [
+    new Ingredient('Apples', 5),
+    new Ingredient('Tomatoes', 10),
+  ];
+
+  getIngredients() {
+    //to return copy of object just slice() which side effect is that, it is returning copy of object;
+    return this.ingredients.slice();
+  }
+
+  addIngredient(ingredient: Ingredient) {
+    this.ingredients.push(ingredient);
+    this.ingredientsChanged.emit(this.ingredients.slice()); // emit new ingredient array and save it to ingredientChanged
+  }
+}
