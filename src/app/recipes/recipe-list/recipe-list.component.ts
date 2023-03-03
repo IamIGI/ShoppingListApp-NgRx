155d6746f048d68e13ae3dffa3b17bf3226a98a9
i.kłusek <J6ZkqwVUn>
2023-03-03 14:41:32 +0100

    added recipeService

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import { Component } from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,22 +8,11 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent {
-  recipes: Recipe[] = [
-    new Recipe(
-      'A test recipe1',
-      'this is simply a test1',
-      'https://ichef.bbci.co.uk/food/ic/food_16x9_832/recipes/british_shakshuka_26737_16x9.jpg'
-    ),
-    new Recipe(
-      'A test recipe2',
-      'this is simply a test2',
-      'https://ichef.bbci.co.uk/food/ic/food_16x9_832/recipes/british_shakshuka_26737_16x9.jpg'
-    ),
-  ];
+  recipes: Recipe[];
 
-  @Output() recipeSelector = new EventEmitter<Recipe>();
+  constructor(private recipeService: RecipeService) {}
 
-  onRecipeClick(selectedRecipe: Recipe) {
-    this.recipeSelector.emit(selectedRecipe);
+  ngOnInit() {
+    this.recipes = this.recipeService.getRecipes();
   }
 }
