ec1055d3e5143ad82fceea582e4bce8fd9773ce9
i.kłusek <J6ZkqwVUn>
2023-03-12 18:39:29 +0100

    Recipes form logic (Angular reactive forms)

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
 
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list/shopping-list.service';
@@ -6,7 +7,8 @@ import { Recipe } from './recipe.model';
 
 @Injectable()
 export class RecipeService {
-  @Injectable()
+  recipesChange = new Subject<Recipe[]>();
+
   private recipes: Recipe[] = [
     new Recipe(
       'A test recipe1',
@@ -39,5 +41,24 @@ export class RecipeService {
 
   addIngredientsToShoppingList(ingredients: Ingredient[]) {
     this.shoppingListService.addIngredients(ingredients);
+  }
+
+  addRecipe(recipe: Recipe) {
+    this.recipes.push(recipe);
+    this.refreshRecipesData();
+  }
+
+  updateRecipe(index: number, newRecipe: Recipe) {
+    this.recipes[index] = newRecipe;
+    this.refreshRecipesData();
+  }
+
+  deleteRecipe(index: number) {
+    this.recipes.splice(index, 1);
+    this.refreshRecipesData();
+  }
+
+  refreshRecipesData() {
+    this.recipesChange.next(this.recipes.slice());
   }
 }
