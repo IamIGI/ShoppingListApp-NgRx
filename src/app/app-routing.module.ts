8d9a8fead27db2e42ee4f268e09fedfb4054b89c
i.kłusek <J6ZkqwVUn>
2023-03-21 11:32:40 +0100

    Feature modules and Shared modules | angular optymizations

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,38 +1,9 @@
-import { Component, NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { AuthGuardService } from './auth/auth-guard.service';
 import { AuthComponent } from './auth/auth.component';
-import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
-import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
-import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
-import { RecipesResolverService } from './recipes/recipes-resolver.service';
-import { RecipesComponent } from './recipes/recipes.component';
-import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 
 const appRoutes: Routes = [
   { path: '', redirectTo: '/recipes', pathMatch: 'full' }, //only redirect when path is exactly the same
-  {
-    path: 'recipes',
-    component: RecipesComponent,
-    canActivate: [AuthGuardService],
-    resolve: [RecipesResolverService], //resolvers will work before given url will be loaded
-    children: [
-      { path: '', component: RecipeStartComponent },
-      { path: 'new', component: RecipeEditComponent },
-      {
-        path: ':id', //routes with dynamic parameters have to be after static urls
-        component: RecipeDetailComponent,
-      },
-      {
-        path: ':id/edit',
-        component: RecipeEditComponent,
-      },
-    ],
-  },
-  {
-    path: 'shopping-list',
-    component: ShoppingListComponent,
-  },
   {
     path: 'auth',
     component: AuthComponent,
