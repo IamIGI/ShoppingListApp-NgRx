03110da4cd8a2610a6f808777bd30757d40a5cb0
i.kłusek <J6ZkqwVUn>
2023-03-22 22:08:11 +0100

    Core Modules (services module)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { AuthComponent } from './auth/auth.component';
 
 const appRoutes: Routes = [
   { path: '', redirectTo: '/recipes', pathMatch: 'full' }, //only redirect when path is exactly the same
-  {
-    path: 'auth',
-    component: AuthComponent,
-  },
 ];
 
 //transform class to angular module
---
+++ src/app/core.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { ShoppingListService } from './shopping-list/shopping-list.service';
+import { RecipeService } from './recipes/recipe.service';
+import { AuthInterceptorService } from './auth/auth-interceptor.service';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+
+@NgModule({
+  providers: [
+    ShoppingListService,
+    RecipeService,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptorService,
+      multi: true,
+    },
+  ], //this services are seen and can be used in all components
+})
+export class CoreModule {}
